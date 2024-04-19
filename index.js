f9b9d391efff47de94faf0780734cd0dfd8c5fdc
arman-mukadam <arman.mukadam2020@gmail.com>
2024-04-19 15:12:22 +0530

    env config

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import express from "express";
 import cors from "cors";
 // import { addTodo,getTodos,} from "./src/controllers/TodoController.js";
-
+import dotenv from "dotenv";
+dotenv.config({ path: "./.env" })
 const app = express();
-const port = 1111
+const port = process.env.PORT
 
 app.use(cors());
 app.use(express.json({ limit: "16kb" }))
